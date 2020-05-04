55aeebd6606c210373a1e46a29423b6474c37170
mitchmorrison2 <mitchm@smu.edu>
2020-05-04 14:06:12 -0700

    finished code, actions next

--- NumPart.py
+++ NumPart.py
@@ -1,14 +1,15 @@
 import itertools
+import time
 
 # read in our number set
 with open('Data.txt', 'r') as f:
     data = f.read().split()
-    # elements = map(int, elements)
 
 """
-# trivial implementation of finding the best way to partition the set
-# @param list of numbers representing set of processes
-# @return two partitioned sets close in sum
+# @desc: trivial implementation to find the best way to partition 
+            the set into two sets of as close to equal sum as possible
+# @param: elements: list of numbers (n < 30)
+# @return: two partitioned sets
 """
 
 
@@ -30,17 +31,15 @@ def trivial_implementation(elements):
         if e not in best_set:
             other_set.append(e)
 
-    # print(best_set, "sum = ", sum(map(int, best_set)))
-    # print(other_set, "other set sum = ", sum(map(int, other_set)))
-
     # return both sets as a list of ints
     return list(map(int, other_set)), list(map(int, best_set))
 
 
 """
-# robust heuristic implementation to find efficient and best way to partition the set
-# @param list of numbers representing set of processes
-# @return two partitioned sets close in sum
+# @desc: robust heuristic implementation to find efficient (and near best)
+            way to partition the set into two sets of equal sum
+# @param: list of integers for two-way partitioning
+# @return: two partitioned sets
 """
 
 
@@ -53,7 +52,6 @@ def robust_implementation(elements):
     # when it is positive, set1 has a larger sum and vice versa
     set_diff = 0
 
-    # print(x)
     var = True
     count = 0
     while count < len(x):
@@ -81,26 +79,43 @@ def robust_implementation(elements):
                 set2.append(x[count])
                 set1.append(x[count+1])
                 set_diff = set_diff + (x[count+1] - x[count])
+
         # increment counter by 2 because moving in pairs
         count = count + 2
 
-    # print("set 1 = ", set1)
-    # print("set 2 = ", set2)
-    # print("set differences = ", set_diff)
-    # print("calc set diff = ", sum(set1) - sum(set2))
+        # edge case for odd amounts of elements in a set
+        if count == len(x) - 1:
+            print(x[count], " ")
+            print(sum(set1), sum(set2))
+            if set_diff > 0:
+                set2.append(x[count])
+                set_diff = set_diff - x[count]
+            else:
+                set1.append(x[count])
+                set_diff = set_diff + x[count]
+            break
 
     # return both sets
     return set1, set2
 
 
 if __name__ == '__main__':
+
     # call trivial implementation
+    start_trivial = time.perf_counter()
     triv_set1, triv_set2 = trivial_implementation(data)
-    # call robust implementation
-    rob_set1, rob_set2 = robust_implementation(data)
+    elapsed_triv = (time.perf_counter() - start_trivial)
 
     print(sorted(triv_set1, reverse=True), sorted(triv_set2, reverse=True))
     print("Trivial remainder:", abs(sum(triv_set1) - sum(triv_set2)))
+    print("Time required for trivial implementation: ", elapsed_triv, " seconds\n\n")
+
+    # call robust implementation
+    start_robust = time.perf_counter()
+    rob_set1, rob_set2 = robust_implementation(data)
+    elapsed_robust = (time.perf_counter() - start_robust)
+
     print(rob_set1, rob_set2)
     print("Robust remainder:", abs(sum(rob_set1) - sum(rob_set2)))
+    print("Time required for robust implementation: ", elapsed_robust, "seconds\n\n")
 
